abbdb43049681eda32d68083acb2772904b0671e
Andrew <1356552@edu.vaniercollege.qc.ca>
2016-02-04 01:46:37 -0500

    small is a real HTML tag, who knew.
    - homepage left : hover popup
    @alex book shit is being weird. but I got 70% there when it comes to make it simple...

--- da2016/js/hoverImageMaps/rollover.js
+++ da2016/js/hoverImageMaps/rollover.js
@@ -1,19 +1,41 @@
+$.fn.maphilight.defaults = {
+    fill: true,
+    fillColor: 'e6d123',
+    fillOpacity: 0.1,
+    stroke: true,
+    strokeColor: 'e6d123',
+    strokeOpacity: 0.45,
+    strokeWidth: 8,
+    fade: true,
+    alwaysOn: false,
+    neverOn: false,
+    groupBy: false,
+    wrapClass: true,
+    shadow: true,
+    shadowX: 0,
+    shadowY: 0,
+    shadowRadius: 6,
+    shadowColor: '000000',
+    shadowOpacity: 0.8,
+    shadowPosition: 'outside',
+    shadowFrom: false
+}
+
     $(document).ready(function($){
-        usemap = $('img[usemap]');
-        usemap.maphilight();
-        usemap.imageMapResize();
+        $('img[usemap]').maphilight();
+        $('map').imageMapResize();
     });
 
 
 $(window).bind('resize', function(e)
 {
-    //window.resizeEvt;
+    window.resizeEvt;
     $(window).resize(function()
     {
         clearTimeout(window.resizeEvt);
         window.resizeEvt = setTimeout(function()
         {
-            jQuery('img[usemap]').maphilight();
+            $('img[usemap]').maphilight();
         }, 250);
     });
 });
