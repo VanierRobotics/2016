58d9f4e3cd925820073d9a38c641e5e1fbedf85c
SkiRacerDude <thereff10@hotmail.com>
2016-02-03 21:21:43 -0500

    Added BioPics and started model

---
+++ includes2016/models/team.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 2016-02-03
+ * Time: 7:40 PM
+ */
+class _Bio extends Model
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function getBios($lang) {
+
+    }
+
+}
