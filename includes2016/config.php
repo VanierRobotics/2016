9e3097599f39cdef6c3ef1080a03f303334ffa1e
SkiRacerDude <thereff10@hotmail.com>
2016-02-06 13:07:42 -0500

    IMPORTANT MODIFICATION TO CONFIG FILE

--- includes2016/config.php
+++ includes2016/config.php
@@ -24,3 +24,5 @@ define('HASH_PW_KEY', '6u355 17\'5 7ru3, 1\'m n07 600d 47 4 0n3-n16h7 574nd
 bu7 1 571ll n33d l0v3 \'c4u53 1\'m ju57 4 m4n
 7h353 n16h75 n3v3r 533m 70 60 70 pl4n
 1 d0n\'7 w4n7 y0u 70 l34v3, w1ll y0u h0ld my h4nd?');
+
+date_default_timezone_set('America/Montreal');
