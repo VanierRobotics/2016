a860a12a8def2abf6398c6d3dc5ce1eb4a6f5897
Andrew <1356552@edu.vaniercollege.qc.ca>
2016-02-07 16:41:30 -0500

    pam anderson is going to love me.

--- includes2016/views/language/index.php
+++ includes2016/views/language/index.php
@@ -1,27 +1,22 @@
 <body>
-
     <div id="wrapper" class="page-wrapper">
+        <link href='https://fonts.googleapis.com/css?family=Satisfy' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="<?=URL?>css/icewall.css"/>
         <script src="<?=URL?>js/parallax/jquery.parallax.min.js"></script>
-        <script src="<?=URL?>js/parallax/icewall.js"></script>
-        <link href='https://fonts.googleapis.com/css?family=Satisfy' rel='stylesheet' type='text/css'>
-        <script src="<?=URL?>js/script.js"></script>
-        <script src="<?=URL?>js/sparkles.js"></script>
+        <script src="<?=URL?>js/parallax/script.js"></script>
+        <script src="<?=URL?>js/parallax/sparkles.js"></script>
 
-        <div class="container" id="content">
+        <div class="container">
             <div id="scene">
-
-                <!-- Take out parrallax for now.... -->
                 <ul class="wallpaper">
                     <li class="layer" id="iceWall" data-depth="0.04"><img src="<?=URL?>images/icewall/layer2.png"/></li>
-                    <li class="layer" id="ground" data-depth="0.1"><img src="<?=URL?>images/icewall/layer3.png"/></li>
+                    <li class="layer" id="ground" data-depth="0.1"><img/></li>
                 </ul>
-
-
-
             </div>
+
             <a href="<?=URL?>en/"><div class="button enButton sparkley">English</div></a>
             <a href="<?=URL?>fr/"><div class="button frButton sparkley">Français</div></a>
+            <div class="vLogo"></div>
         </div>
     </div>
 </body>
