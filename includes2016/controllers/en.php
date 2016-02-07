752ccc102e24c2513adc0eb7b36432f2db9b27d8
Andrew <1356552@edu.vaniercollege.qc.ca>
2016-02-07 11:33:46 -0500

    somebody keeps erasing my changes.

--- includes2016/controllers/en.php
+++ includes2016/controllers/en.php
@@ -14,6 +14,7 @@ class en extends Controller
         //self::checkMember();
         $this->view->title = 'Vanier Robotics 2016';
         $this->view->language = 'en';
+        $this->lang = 0;
     }
 
     public function evanshit(){
@@ -31,6 +32,8 @@ class en extends Controller
 
     public function robot(){
         $this->view->book = 'robot';
+        $this->model = $this->getModel('Book');
+        $this->view->texts = $this->model-> getBookPage($this->lang, 'GAME');
         $this->view->render('book/index');
     }
 
@@ -41,6 +44,8 @@ class en extends Controller
 
     public function vanier(){
         $this->view->book = 'vanier';
+        $this->model = $this->getModel('Book');
+        $this->view->texts = $this->model-> getTeamPage($this->lang, 'JOURNALISM', 'VIDEO');
         $this->view->render('book/index');
     }
 
