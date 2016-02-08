9ced8ae8a47c4b523cd3775f804047861dfd1a38
SkiRacerDude <thereff10@hotmail.com>
2016-02-08 00:06:05 -0500

    HTML Validation Round4

--- includes2016/views/journalism/index.php
+++ includes2016/views/journalism/index.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="<?=URL?>css/journalismBooks.css">
+<link rel="stylesheet" href="<?= URL ?>css/journalismBooks.css">
 
 
 <div class="container">
@@ -7,62 +7,50 @@
     </header>
     <div class="main">
         <ul id="bk-list" class="bk-list clearfix">
-            <a href="./Video" >
-                <li>
-                    <div class="bk-book book-1">
-                        <div class="bk-front">
-                            <div class="bk-cover"></div>
-                        </div>
-                        <div class="bk-back"> </div>
-                        <div class="bk-right"></div>
-                        <div class="bk-left"></div>
-                        <div class="bk-top"></div>
+            <li onclick="window.location = '<?= $link ?>/Video';">
+                <div class="bk-book book-1">
+                    <div class="bk-front">
+                        <div class="bk-cover"></div>
                     </div>
-                </li>
-            </a>
-
-            <a href="./Build" >
-                <li>
-                    <div class="bk-book book-2">
-                        <div class="bk-front">
-                            <div class="bk-cover"></div>
-                        </div>
-                        <div class="bk-back"> </div>
-                        <div class="bk-right"></div>
-                        <div class="bk-left"></div>
-                        <div class="bk-top"></div>
+                    <div class="bk-back"></div>
+                    <div class="bk-right"></div>
+                    <div class="bk-left"></div>
+                    <div class="bk-top"></div>
+                </div>
+            </li>
+            <li onclick="window.location = '<?= $link ?>/Build';">
+                <div class="bk-book book-2">
+                    <div class="bk-front">
+                        <div class="bk-cover"></div>
                     </div>
-                </li>
-            </a>
-
-            <a href="./Web" >
-                <li>
-                    <div class="bk-book book-3">
-                        <div class="bk-front">
-                            <div class="bk-cover"></div>
-                        </div>
-                        <div class="bk-back"> </div>
-                        <div class="bk-right"></div>
-                        <div class="bk-left"></div>
-                        <div class="bk-top"></div>
+                    <div class="bk-back"></div>
+                    <div class="bk-right"></div>
+                    <div class="bk-left"></div>
+                    <div class="bk-top"></div>
+                </div>
+            </li>
+            <li onclick="window.location = '<?= $link ?>/Web';">
+                <div class="bk-book book-3">
+                    <div class="bk-front">
+                        <div class="bk-cover"></div>
                     </div>
-                </li>
-            </a>
-
-            <a href="./Kiosk" >
-                <li>
-                    <div class="bk-book book-4">
-                        <div class="bk-front">
-                            <div class="bk-cover"></div>
-                        </div>
-                        <div class="bk-back"> </div>
-                        <div class="bk-right"></div>
-                        <div class="bk-left"></div>
-                        <div class="bk-top"></div>
+                    <div class="bk-back"></div>
+                    <div class="bk-right"></div>
+                    <div class="bk-left"></div>
+                    <div class="bk-top"></div>
+                </div>
+            </li>
+            <li onclick="window.location = '<?= $link ?>/Kiosk';">
+                <div class="bk-book book-4">
+                    <div class="bk-front">
+                        <div class="bk-cover"></div>
                     </div>
-                </li>
-            </a>
-
+                    <div class="bk-back"></div>
+                    <div class="bk-right"></div>
+                    <div class="bk-left"></div>
+                    <div class="bk-top"></div>
+                </div>
+            </li>
         </ul>
         <div class="bookshelf"></div>
     </div>
